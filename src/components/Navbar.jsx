8aa49725c60b177a6eb7d62c19aaad690a05a6de
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-07-03 13:47:38 +0530

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-gradient-to-r from-indigo-800 to-purple-800 shadow-md">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center text-white">
-        <a href='/contact' target="_blank" rel="noopener noreferrer" className="hover:text-yellow-300"><h1 className="text-xl font-bold">Soumya Ranjan</h1></a>
+        <a href='https://www.linkedin.com/in/soumya-ranjan-bhanja-270644247' target="_blank" rel="noopener noreferrer" className="hover:text-yellow-300"><h1 className="text-xl font-bold">Soumya Ranjan</h1></a>
         <div className="space-x-6">
           <a href="https://github.com/SoumyaRanjanBhanja1" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-300">GitHub</a>
           <a href="https://www.linkedin.com/in/soumya-ranjan-bhanja-270644247" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-300">LinkedIn</a>
